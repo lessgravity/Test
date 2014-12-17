5fca997c8e2c5a207ee0a02e2cdc1de65b0953eb
Less Gravity <less.gravity@outlook.com>
2014-12-18 00:11:57 +0100

    added basic network client stuff

--- TestNetwork.Network/TestNetwork.Network.Client/NetworkClient.cs
+++ TestNetwork.Network/TestNetwork.Network.Client/NetworkClient.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Net;
 using System.Net.Sockets;
+using System.Runtime.CompilerServices;
 using System.Threading;
 using TestNetwork.Network.Packets;
 
@@ -19,8 +21,12 @@ namespace TestNetwork.Network.Client
         private NetworkStream _networkStream;
         private Thread _networkThread;
 
+        private DateTime _nextPlayerUpdate;
+
         private readonly ConcurrentQueue<IPacket> _packetQueue;
-        private readonly IDictionary<Type, Action<NetworkClient, IPacket>> _packetHandlers; 
+        private readonly IDictionary<Type, Action<NetworkClient, IPacket>> _packetHandlers;
+
+        public bool IsSpawned { get; private set; }
 
         public void Connect(IPEndPoint endPoint)
         {
@@ -66,6 +72,17 @@ namespace TestNetwork.Network.Client
         {
         }
 
+        private void HandlePacket(IPacket packet)
+        {
+            var packetType = packet.GetType();
+            Action<NetworkClient, IPacket> packetHandler;
+            if (_packetHandlers.TryGetValue(packetType, out packetHandler))
+            {
+                Debug.WriteLine("Handle Packet: {0}", packetType.Name);
+                packetHandler(this, packet);
+            }
+        }
+
         public NetworkClient(NetworkSession networkSession)
         {
             _networkLock = new object();
@@ -78,10 +95,71 @@ namespace TestNetwork.Network.Client
         {
             while (true)
             {
+
+                if (IsSpawned && _nextPlayerUpdate < DateTime.Now)
+                {
+                    /*
+                    lock (_positionLock)
+                    {
+                        SendPacket(new PlayerPacket(true));
+                        if (_positionChanged)
+                        {
+                            SendPacket(new PlayerPositionPacket(XYZ));
+                            _positionChanged = false;
+                        }
+                    }
+                     * */
+                }
+                while (_packetQueue.Count > 0)
+                {
+                    IPacket packet;
+                    if (!_packetQueue.TryDequeue(out packet))
+                    {
+                        continue;
+                    }
+                    try
+                    {
+                        _networkManager.WritePacket(packet, PacketDirection.ToServer);
+                        if (packet is DisconnectPacket)
+                        {
+                            return;
+                        }
+                    }
+                    catch
+                    {
+                    }
+                }
+                var readTimeOut = DateTime.Now.AddMilliseconds(20);
+                while (_networkStream.DataAvailable && DateTime.Now < readTimeOut)
+                {
+                    try
+                    {
+                        var packet = _networkManager.ReadPacket(PacketDirection.ToClient);
+                        HandlePacket(packet);
+                        if (packet is DisconnectPacket)
+                        {
+                            Debug.WriteLine(((DisconnectPacket) packet).Reason);
+                            return;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine(ex.Message);
+                    }
+                }
                 _networkResetEvent.Set();
                 _networkResetEvent.Reset();
                 Thread.Sleep(1);
             }
+        }
+
+        public void SendPacket(IPacket packet)
+        {
+            if (packet == null)
+            {
+                throw new ArgumentNullException("packet");
+            }
+            _packetQueue.Enqueue(packet);
         }
     }
 }
